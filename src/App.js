98767bcb6cfd556452095a4f333d326b7e83e89e
Prajwaldule <prajwaldule58@gmail.com>
2023-07-12 00:36:12 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div>
+       <div className="App">
+          <TodoWrapper  />
+       </div>
+    </div>
+  );
+}
+
+export default App;
